f8c15ff319f87a4e935e728f1042f5b7c13577f0
Giulia Alciati <giulia.alciati@hotmail.com>
2025-05-13 15:10:32 -0300

    Adicionando carrossel de serviços

---
+++ assets/JavaScript/calendar.js
@@ -0,0 +1,48 @@
+const currentDate = document.querySelector(".current-date"),
+daysTag = document.querySelector(".days"),
+prevNextIcon = document.querySelectorAll(".icons span");
+
+let date = new Date(),
+currYear = date.getFullYear(),
+currMonth = date.getMonth();
+
+const months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+const renderCalendar = () => {
+    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(),
+    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(),
+    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
+    let liTag = "";
+    
+    
+    for (let i = firstDayofMonth; i > 0; i--) {
+        liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+    }
+
+    for (let i = 1; i <= lastDateofMonth; i++) {
+        let isToday = i === date.getDate() && currMonth === new Date().getMonth()
+                    && currYear === new Date().getFullYear() ? "active" : "";
+        liTag += `<li class = "${isToday}">${i}</li>`;
+    }
+
+    currentDate.innerText = `${months[currMonth]} ${currYear}`;
+    daysTag.innerHTML = liTag;
+}
+
+renderCalendar();
+
+prevNextIcon.forEach(icon => {
+    icon.addEventListener("click", () => {
+        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+        
+        if (currMonth < 0) {
+            currMonth = 11;
+            currYear -= 1;
+        } else if (currMonth > 11) {
+            currMonth = 0;
+            currYear += 1;
+        }
+
+        renderCalendar();
+    });
+});
